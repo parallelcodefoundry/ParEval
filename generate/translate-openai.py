9911eb28bc050cee809f47ada55d5447e0b1315f
Daniel Nichols <dando18studios@gmail.com>
2024-02-18 19:11:31 -0500

    Translation (#17)
    * finalize prompts
    
    * translation scripts
    
    * gpt translate outputs
    
    * add translation outputs
    
    * translation results

---
+++ generate/translate-openai.py
@@ -0,0 +1,270 @@
+""" Get the model outputs from OpenAI's API.
+    author: Daniel Nichols
+    date: January 2024
+"""
+# std imports
+from argparse import ArgumentParser
+import json
+import os
+import re
+import time
+from typing import Optional
+
+# tpl imports
+from tqdm import tqdm
+from openai import OpenAI
+
+""" Prompt template: """
+SYSTEM_TEMPLATE = """You are a helpful coding assistant.
+You are helping a programmer translate {src_model} code to {dst_model} code. Write only the body of the function and put it in a markdown code block.
+Do not write any other code or explanations.
+"""
+
+PROMPT_TEMPLATE = """Here is an implementation of the {function_name} function in {src_model}.
+```cpp
+{src_example}
+```
+
+Rewrite {function_name} to use {dst_model} for parallelism. Write the function body in a markdown code block and nothing else.
+```cpp
+{dst_prompt}
+```
+"""
+
+EXECUTION_MODEL_CLEAN_NAME_MAP = {
+    "serial": "Serial",
+    "omp": "OpenMP",
+    "mpi": "MPI",
+    "mpi+omp": "MPI+OpenMP",
+    "kokkos": "Kokkos",
+    "cuda": "CUDA",
+    "hip": "HIP"
+}
+
+
+def get_args():
+    parser = ArgumentParser(description=__doc__)
+    parser.add_argument("-m", "--model", choices=["gpt-4-1106-preview", "gpt-3.5-turbo-1106"], required=True, help="The model to use.")
+    parser.add_argument("-p", "--prompts", type=str, required=True, help="Path to prompts json")
+    parser.add_argument("-o", "--output", type=str, required=True, help="Path to output json")
+    parser.add_argument("--api-key", type=str, help="OpenAI API key. " +
+        "If not provided, then uses environment variable OPENAI_API_KEY.")
+    parser.add_argument("--openai-organization", type=str, help="OpenAI organization. " +
+        "If not provided, then uses environment variable OPENAI_ORGANIZATION.")
+    parser.add_argument("--max-requests", type=int, help="If provided, then only makes this many requests.")
+    parser.add_argument("--max-tokens-per-second", help="Limit the rate of token generation.")
+    parser.add_argument("--max-requests-per-second", help="Limit the rate of request generation.")
+    parser.add_argument("--dry", action="store_true", help="If provided, then don't make any requests.")
+    parser.add_argument("--overwrite", action="store_true", help="If provided, then overwrite outputs already in file.")
+    parser.add_argument("--temperature", type=float, default=0.2, help="The temperature to use for sampling.")
+    parser.add_argument("--top-p", type=float, default=0.95, help="The top p to use for sampling.")
+    parser.add_argument("--max-new-tokens", type=int, default=1024, help="The maximum number of tokens to generate.")
+    parser.add_argument("--num-samples-per-prompt", type=int, default=20, help="The number of samples to generate per prompt.")
+    return parser.parse_args()
+
+
+def get_env_var(name: str) -> str:
+    """ Get an environment variable. """
+    if name not in os.environ:
+        raise ValueError(f"Environment variable {name} not set.")
+    return os.environ[name]
+
+GPU_FUNCTION_NAME_PATTERN = re.compile(r"__global__ void ([a-zA-Z0-9_]+)\(")
+CPU_FUNCTION_NAME_PATTERN = re.compile(r"\s*[a-zA-Z_]+ ([a-zA-Z0-9_]+)\(")
+def get_function_name(prompt: str, execution_model: str) -> str:
+    if execution_model in ['cuda', 'hip']:
+        match = GPU_FUNCTION_NAME_PATTERN.match(prompt.splitlines()[-1])
+    else:
+        match = CPU_FUNCTION_NAME_PATTERN.match(prompt.splitlines()[-1])
+    if match is None:
+        raise ValueError(f"Could not find function name in prompt: {prompt}")
+    return match.group(1)
+
+def get_max_tokens_per_second(model: str) -> Optional[int]:
+    """ rates limites as of January 2024 """
+    if model == "gpt-3.5-turbo-1106":
+        tokens_per_minute = 60_000
+        return tokens_per_minute / 60
+    elif model == "gpt-4-1106-preview":
+        tokens_per_minute = 150_000
+        return tokens_per_minute / 60
+    else:
+        return None
+    
+def get_max_requests_per_second(model: str) -> Optional[int]:
+    """ rates limites as of January 2024 """
+    if model == "gpt-3.5-turbo-1106":
+        requests_per_minute = 3_500
+        return requests_per_minute / 60
+    elif model == "gpt-4-1106-preview":
+        requests_per_minute = 500
+        return requests_per_minute / 60
+    else:
+        return None
+
+def get_max_requests_per_day(model: str) -> Optional[int]:
+    """ rates limites as of January 2024 """
+    if model == "gpt-3.5-turbo-1106":
+        return 10_000
+    elif model == "gpt-4-1106-preview":
+        return 10_000
+    else:
+        return None
+
+def postprocess(prompt: str, output: str) -> str:
+    """ Postprocess the output. """
+    # remove leading ```, ```cpp, and trailing ```
+    output = output.strip().lstrip("```cpp").lstrip("```").rstrip("```")
+
+    # remove prompt if it included it
+    if output.startswith(prompt):
+        output = output[len(prompt):]
+
+    return output
+
+def main():
+    args = get_args()
+
+    # get the prompts
+    with open(args.prompts, 'r') as prompts_json:
+        prompts = json.load(prompts_json)
+
+    # read in outputs
+    if not args.overwrite and os.path.exists(args.output):
+        with open(args.output, 'r') as output_json:
+            outputs = json.load(output_json)
+
+        # copy existing outputs into prompts
+        copy_count = 0
+        for prompt in prompts:
+            for o in outputs:
+                if o["prompt"] == prompt["prompt"] and \
+                   o["translation_prompt"] == prompt["translation_prompt"] and \
+                   o["translation_src_model"] == prompt["translation_src_model"] and \
+                   o["translation_dst_model"] == prompt["translation_dst_model"] and \
+                   o["name"] == prompt["name"] and \
+                   o["parallelism_model"] == prompt["parallelism_model"] and \
+                   "outputs" in o and \
+                   len(o["outputs"]) == args.num_samples_per_prompt and \
+                   o["temperature"] == args.temperature and \
+                   o["top_p"] == args.top_p:
+                    for col in ["temperature", "top_p", "do_sample", "max_new_tokens", "outputs"]:
+                        prompt[col] = o[col]
+                    copy_count += 1
+                    break
+        print(f"Copied {copy_count} existing outputs.")
+
+    # get the keys
+    api_key = args.api_key or get_env_var("OPENAI_API_KEY")
+    organization = args.openai_organization or get_env_var("OPENAI_ORGANIZATION")
+
+    # create the client
+    client = OpenAI(api_key=api_key, organization=organization)
+
+    # generation metadata
+    MAX_TOKENS_PER_SECOND = args.max_tokens_per_second or get_max_tokens_per_second(args.model)
+    MAX_REQUESTS_PER_SECOND = args.max_requests_per_second or get_max_requests_per_second(args.model)
+    MAX_REQUESTS = args.max_requests or get_max_requests_per_day(args.model)
+
+    # generate outputs
+    request_counter = 0
+    request_rate_counter = 0
+    token_counter = 0
+    token_rate_counter = 0
+    token_timer = time.time()
+    request_timer = time.time()
+    for prompt in tqdm(prompts, desc="Generating outputs"):
+        # see if we can skip this
+        if not args.overwrite and "outputs" in prompt:
+            continue
+
+        # get the prompt
+        original_prompt = prompt["prompt"]
+        function_name = prompt["translation_function_name"]
+        src_model = prompt["translation_src_model"]
+        src_model_clean_name = EXECUTION_MODEL_CLEAN_NAME_MAP[src_model]
+        dst_model = prompt["translation_dst_model"]
+        dst_model_clean_name = EXECUTION_MODEL_CLEAN_NAME_MAP[dst_model]
+
+        system_text = SYSTEM_TEMPLATE.format(src_model=src_model_clean_name, dst_model=dst_model_clean_name)
+        prompt_text = PROMPT_TEMPLATE.format(
+            src_model=src_model_clean_name,
+            src_example=prompt["translation_src_example"],
+            dst_model=dst_model_clean_name,
+            function_name=function_name,
+            dst_prompt=original_prompt
+        )
+
+        # generate the outputs
+        if args.dry:
+            print("system", system_text)
+            print("prompt", prompt_text, "\n")
+            continue
+
+        # set metadata
+        prompt["temperature"] = args.temperature
+        prompt["top_p"] = args.top_p
+        prompt["do_sample"] = True
+        prompt["max_new_tokens"] = args.max_new_tokens
+
+        # generate the outputs
+        completion = client.chat.completions.create(
+            model=args.model,
+            messages=[
+                {"role": "system", "content": system_text},
+                {"role": "user", "content": prompt_text}
+            ],
+            max_tokens=args.max_new_tokens,
+            temperature=args.temperature,
+            top_p=args.top_p,
+            stream=False,
+            n=args.num_samples_per_prompt
+        )
+
+        outputs = [c.message.content for c in completion.choices]
+        outputs = [postprocess(original_prompt, o) for o in outputs]
+        prompt["outputs"] = outputs
+
+        # update counters
+        request_counter += 1
+        request_rate_counter += 1
+        token_counter += completion.usage.total_tokens
+        token_rate_counter += completion.usage.total_tokens
+
+        # check if we should stop
+        if MAX_REQUESTS is not None and request_counter >= MAX_REQUESTS:
+            print(f"Stopping after {request_counter} requests.")
+            break
+    
+        # check if we should sleep
+        tokens_per_second = token_rate_counter / (time.time() - token_timer)
+        if MAX_TOKENS_PER_SECOND is not None and tokens_per_second > (MAX_TOKENS_PER_SECOND*0.9):
+            sleep_time = 30
+            print(f"Sleeping for {sleep_time} seconds.")
+            time.sleep(sleep_time)
+            token_timer = time.time()
+            token_rate_counter = 0
+        
+        requests_per_second = request_rate_counter / (time.time() - request_timer)
+        if MAX_REQUESTS_PER_SECOND is not None and requests_per_second > (MAX_REQUESTS_PER_SECOND*0.95):
+            sleep_time = 60
+            print(f"Sleeping for {sleep_time} seconds.")
+            time.sleep(sleep_time)
+            request_timer = time.time()
+            request_rate_counter = 0
+
+        # write intermediate outputs
+        with open(args.output, 'w') as output_json:
+            json.dump(prompts, output_json, indent=2)
+
+    # summary stats
+    print(f"Submitted {request_counter} requests.")
+    print(f"Used {token_counter} tokens.")
+
+    # write outputs
+    with open(args.output, 'w') as output_json:
+        json.dump(prompts, output_json, indent=2)
+    
+
+if __name__ == "__main__":
+    main()
---
+++ generate/translate.py
@@ -0,0 +1,155 @@
+# std imports
+import argparse
+import json
+import os
+import sys
+import time
+from tqdm import tqdm
+
+# tpl imports
+import torch
+from transformers import pipeline
+
+# local imports
+from utils import BalancedBracketsCriteria, PromptDataset, clean_output, get_inference_config
+
+
+""" Parse command line arguments """
+parser = argparse.ArgumentParser(description='Generate code')
+parser.add_argument('--prompts', required=True, help='Path to the prompt JSON file')
+parser.add_argument('--model', required=True, help='Path to the language model')
+parser.add_argument('--output', required=True, help='Path to the output JSON file')
+parser.add_argument('--restart', action='store_true', help='Restart generation from scratch (default: False)')
+parser.add_argument('--cache', help='JSONL file to cache intermediate results in. Will be restored from if it ' +
+    'already exists and --restart is not specified')
+parser.add_argument('--restore_from', help='JSON file to restore old results from. Will be restored from ' +
+    'if it already exists and --restart is not specified. Is different from --cache in that it is a JSON file, not a ' +
+    'JSONL file, and it is only used to restore old results where the prompt is equivalent. Cached results are ' +
+    'prioritized over restored results.')
+parser.add_argument('--max_new_tokens', type=int, default=1024, help='Maximum number of new tokens to generate (default: 1024)')
+parser.add_argument('--num_samples_per_prompt', type=int, default=50, help='Number of code samples to generate (default: 50)')
+parser.add_argument('--temperature', type=float, default=0.2, help='Temperature for controlling randomness (default: 0.2)')
+parser.add_argument('--top_p', type=float, default=0.95, help='Top p value for nucleus sampling (default: 0.95)')
+parser.add_argument('--do_sample', action='store_true', help='Enable sampling (default: False)')
+parser.add_argument('--batch_size', type=int, default=16, help='Batch size for generation (default: 8)')
+parser.add_argument('--prompted', action='store_true', help='Use prompted generation. See StarCoder paper (default: False)')
+args = parser.parse_args()
+
+""" Load prompts """
+with open(args.prompts, 'r') as json_file:
+    prompts = json.load(json_file)
+
+""" Load existing responses if they exist """
+if not args.restart and os.path.exists(args.cache):
+    with open(args.cache, 'r') as jsonl_file:
+        responses = [json.loads(line) for line in jsonl_file]
+    
+    # remove prompt from prompts if it is in responses and has an 'output' value with at least 1 entry
+    original_len = len(prompts)
+    prompts = [p for p in prompts if 
+                not any(p["name"] == r["name"] and 
+                        p["parallelism_model"] == r["parallelism_model"] and
+                        p["translation_prompt"] == r["translation_prompt"] and 
+                        args.temperature == r["temperature"] and 
+                        args.prompted == r["prompted"] and
+                        args.num_samples_per_prompt == len(r["outputs"])
+                        for r in responses)]
+    print(f"[cache] Skipping {original_len - len(prompts)} prompts that already have responses")
+
+""" Load existing responses if they exist """
+if not args.restart and args.restore_from and os.path.exists(args.restore_from):
+    with open(args.restore_from, 'r') as json_file:
+        restored_responses = json.load(json_file)
+    
+    # remove prompt from prompts if it is in responses and has an 'output' value with at least 1 entry
+    original_len = len(prompts)
+    responses_to_keep = []
+    prompts_without_existing_responses = []
+    for p in prompts:
+        for r in restored_responses:
+            if p["name"] == r["name"] and \
+                p["parallelism_model"] == r["parallelism_model"] and \
+                p["translation_prompt"] == r["translation_prompt"] and \
+                args.temperature == r["temperature"] and \
+                args.prompted == r["prompted"] and \
+                args.num_samples_per_prompt == len(r["outputs"]):
+                responses_to_keep.append(r)
+                break
+        else:
+            prompts_without_existing_responses.append(p)
+    prompts = prompts_without_existing_responses
+    print(f"[restore_from] Skipping {original_len - len(prompts)} prompts that already have responses. " +
+        f"{len(prompts)} prompts left.")
+
+    # write restored responses to cache
+    if args.cache is not None:
+        with open(args.cache, 'a') as jsonl_file:
+            for response in responses_to_keep:
+                jsonl_file.write(json.dumps(response) + "\n")
+            print(f"[restore_from] Wrote {len(responses_to_keep)} restored responses to cache")
+
+
+""" Initialize inference config """
+inference_config = get_inference_config(args.model, prompted=args.prompted)
+
+# to use a torch.utils.data.DataSet with the HuggingFace pipeline, we need to flatten out the prompts
+# and repeat them for however many samples we want to generate per prompt
+prompts_repeated = [p for p in prompts for _ in range(args.num_samples_per_prompt)]
+
+""" Initialize HuggingFace pipeline for generation """
+generator = pipeline(model=args.model, torch_dtype=inference_config.get_dtype(), device=0)
+inference_config.init_padding(generator.tokenizer)
+
+""" Create a prompt data set to pass to generate method """
+prompt_dataset = PromptDataset([inference_config.format_prompt(p["translation_prompt"]) for p in prompts_repeated])
+generated_outputs = generator(
+    prompt_dataset,
+    max_new_tokens=args.max_new_tokens,
+    do_sample=args.do_sample,
+    temperature=args.temperature,
+    top_p=args.top_p,
+    pad_token_id=inference_config.get_pad_token_id(generator.tokenizer),
+    eos_token_id=inference_config.get_eos_token_id(generator.tokenizer),
+    batch_size=args.batch_size,
+)
+
+""" Iterate over prompts and generate code """
+if not args.restart and args.cache is not None and os.path.exists(args.cache):
+    with open(args.cache, 'r') as jsonl_file:
+        responses = [json.loads(line) for line in jsonl_file]
+        responses = [r for r in responses if r["temperature"] == args.temperature and r["prompted"] == args.prompted
+                        and args.num_samples_per_prompt == len(r["outputs"])
+                        and any(p["name"] == r["name"] and p["translation_prompt"] == r["translation_prompt"] and p["parallelism_model"] == r["parallelism_model"] for p in prompts)]
+else:
+    responses = []
+cur_prompt = None
+start_time = time.time()
+total_tokens = 0
+for idx, (prompt, output) in tqdm(enumerate(zip(prompts_repeated, generated_outputs)), total=len(prompts_repeated), desc="Generating code", file=sys.stdout):
+    if idx % args.num_samples_per_prompt == 0:
+        cur_prompt = prompt.copy()
+        cur_prompt.update({"temperature": args.temperature, "top_p": args.top_p, "do_sample": args.do_sample, "max_new_tokens": args.max_new_tokens, "prompted": args.prompted})
+        cur_prompt["outputs"] = []
+        prompt_str = cur_prompt["translation_prompt"]
+
+    total_tokens += len(generator.tokenizer.encode(output[0]["generated_text"]))
+    cleaned_output = clean_output(output[0]["generated_text"], prompt_str)
+    cur_prompt["outputs"].append(cleaned_output)
+
+    if idx % args.num_samples_per_prompt == args.num_samples_per_prompt - 1:
+        responses.append(cur_prompt)
+
+        if not args.restart and args.cache is not None:
+            with open(args.cache, 'a') as jsonl_file:
+                jsonl_file.write(json.dumps(cur_prompt) + "\n")
+
+    if idx != 0 and idx % args.num_samples_per_prompt == 0:
+        print(f"Tokens per second: {total_tokens / (time.time() - start_time):.2f}")
+
+end_time = time.time()
+tokens_per_second = total_tokens / (end_time - start_time)
+print(f"Generated {len(responses)} code samples in {end_time - start_time:.2f} seconds ({tokens_per_second:.2f} tokens per second)")
+
+""" Save responses to JSON file """
+with open(args.output, 'w') as output_file:
+    json.dump(responses, output_file, indent=4)
